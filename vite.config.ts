48a635ced5ae567bc53e669b08cd96ce7ff0944a
Sheriff24586 <115365416+Sheriff24586@users.noreply.github.com>
2026-06-08 16:42:21 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  publicDir: ".",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "."),
